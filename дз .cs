b8f63aca6eff9c86c3f44566c8bc994cdaf4a19b
Alexander <20alexxander05@gmail.com>
2025-04-02 10:56:32 +0300

    Update дз .cs

--- дз .cs
+++ дз .cs
@@ -1,25 +1,45 @@
 using System;
 
-public class Solution {
-    public int[] SingleNumber(int[] nums) {
+class Program
+{
+    static void Main()
+    {
+        // Пример входного массива
+        int[] nums = { 1, 2, 1, 3, 2, 5 };
+
+        // Находим два уникальных числа
+        int[] result = SingleNumber(nums);
+
+        // Выводим результат
+        Console.WriteLine("Уникальные числа: [" + string.Join(", ", result) + "]");
+    }
+
+    public static int[] SingleNumber(int[] nums)
+    {
         // Получаем XOR всех элементов (это будет a ^ b)
         int xorResult = 0;
-        foreach (int num in nums) {
+        foreach (int num in nums)
+        {
             xorResult ^= num;
         }
 
         // Находим любой установленный бит в xorResult (возьмём самый младший)
         int diffBit = 1;
-        while ((xorResult & diffBit) == 0) {
+        while ((xorResult & diffBit) == 0)
+        {
             diffBit <<= 1;
         }
 
         // Разделяем числа на две группы и находим a и b
         int a = 0, b = 0;
-        foreach (int num in nums) {
-            if ((num & diffBit) != 0) {
+        foreach (int num in nums)
+        {
+            if ((num & diffBit) != 0)
+            {
                 a ^= num;
-            } else {
+            }
+            else
+            {
                 b ^= num;
             }
         }
